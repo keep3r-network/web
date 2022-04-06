9e419d169ff376f4974900abe7b25671acf3f3bb
0xGorilla <84932007+0xGorilla@users.noreply.github.com>
2022-04-06 10:10:58 +0200

    fix: thekeep3r.network to keep3r.network

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,8 +3,8 @@
     <nav class="navigation">
       <ul>
         <li><router-link to="/">Keep3r V1</router-link></li>
-        <li><a href="//thekeep3r.network">Keep3r V2</a></li>
-        <li><a href="//v1.docs.thekeep3r.network" target="_blank">Docs</a></li>
+        <li><a href="//keep3r.network">Keep3r V2</a></li>
+        <li><a href="//v1.docs.keep3r.network" target="_blank">Docs</a></li>
       </ul>
     </nav>
   </div>
